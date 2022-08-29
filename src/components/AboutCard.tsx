06d76b64d3600c69d2f9376684ca75a62990ff49
chon <balint.ducsai@gmail.com>
2022-08-29 22:26:48 +0200

    improve landing page

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,13 +1,41 @@
-// THINK design a better landing page design
-export default function AboutCard() {
+import { useState } from 'react';
+import closeIcon from '../assets/close.svg';
+
+type AboutCardProps = {
+  title?: string;
+  content?: string;
+};
+
+// TODO refactor this!!
+export default function AboutCard({ title, content }: AboutCardProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    console.log('close');
+    setIsOpen(false);
+  };
+
   return (
-    <div className="bg-white rounded-3xl border-black border-solid border-4  p-6  ">
-      <h1 className="font-bold">About Title</h1>
-      <p className="ml-2 multiline-truncate-4">
-        About Content Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt laboriosam dolore molestiae
-        assumenda neque ipsam, cupiditate similique, dicta amet odit maiores doloribus rem illo, repellendus quaerat aut.
-        Magnam, libero iure.
-      </p>
+    <div className={isOpen ? 'fixed inset-2 flex z-50 !m-0' : ''}>
+      {isOpen && (
+        <button className="absolute top-2 right-2" onClick={() => close()}>
+          <img className="w-12" src={closeIcon} alt="close-button" />
+        </button>
+      )}
+
+      <div className="bg-white rounded-3xl border-black border-solid border-4 p-6 overflow-hidden" onClick={open}>
+        <div className="overflow-auto h-full">
+          <h1 className="font-bold">{title || 'Title'}</h1>
+          <p className={`mx-2 ${isOpen || 'multiline-truncate-4'}`}>
+            {content ||
+              'About Content Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt laboriosam dolore molestiae assumenda neque ipsam, cupiditate similique, dicta amet odit maiores doloribus rem illo, repellendus quaerat aut. Magnam, libero iure. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia tempora quos possimus sint eaque voluptatum, temporibus ipsam, repudiandae in accusamus corrupti eveniet. Praesentium error doloribus vitae. Velit magnam pariatur laboriosam! Iste magnam cumque dolore at natus minima molestiae quidem nisi voluptatum commodi et impedit sint eos voluptate laboriosam voluptas, magni expedita quasi consequatur debitis a laudantium, dicta similique harum. Doloremque? Ipsa nam mollitia ullam deserunt sequi cum tenetur delectus ratione voluptatibus similique earum dolorum ab molestiae maiores ut dicta, debitis repellat voluptatem voluptates. Quis porro sapiente facere eveniet animi sint. Sit laboriosam libero fuga cupiditate ut porro perferendis possimus dolore animi, recusandae cumque distinctio aspernatur aliquam odio numquam nemo quaerat? Enim voluptate exercitationem laudantium, modi assumenda culpa dolorum est quam. Neque explicabo modi quaerat veritatis et natus hic eum qui inventore iusto? Iste magni temporibus harum, dignissimos eligendi, aut eaque ut perferendis sed obcaecati aliquam mollitia necessitatibus culpa error. Soluta.'}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,10 +7,18 @@ export default function LandingPage() {
       <div className="flex justify-center">
         <img src={logo} alt="cover" className="m-12" />
       </div>
-      <div className="grid auto-rows-auto -space-y-24 md:space-y-0 md:grid-cols-3 md:gap-4  m-2 ">
-        <AboutCard />
-        <AboutCard />
-        <AboutCard />
+
+      <div className="grid auto-rows-auto -space-y-32 md:space-y-0 md:grid-cols-3 md:gap-4  m-4">
+        <AboutCard
+          title="A vállalkozásról"
+          content="Üzletem nincs, ezért postai utánvétellel szállítok, melynek költsége a megrendelőt terheli. A sárkányrendelés történhet on-line módon, faxon, vagy telefonon.
+          A postai utánvételes szállításhoz szükség van a megrendelő pontos nevére, címére (irányítószámmal), továbbá a rendelt sárkány nevére, mennyiségére. Ha a számlázási cím nem azonos a megrendelőével, kérem azt is pontosan megjelölni!
+          Személyesen Nagykovácsiban (63-as BKV busszal megközelíthető) is vásárolhat.
+          Sárkányaim repülési és nyolc napos pénzvisszafizetési garanciával kaphatók. A visszaküldés költsége a vevőt terheli.
+          Minden érdeklődőnek egy óra ingyenes oktatás sárkány biztosításával - vásárlási kötelezettség nélkül - Nagykovácsiban. (Telefonos időpont egyeztetés után.)
+          Mindenkinek kellemes sárkányeresztést kíván a www.papirsarkany.hu egyéni vállalkozás tulajdonosa:
+          Ducsai Barnabás"
+        />
         <AboutCard />
         <AboutCard />
         <AboutCard />
