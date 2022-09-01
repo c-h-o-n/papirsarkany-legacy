f78949b96611b61ed0d520f6302e3930b64bfb24
chon <balint.ducsai@gmail.com>
2022-09-01 13:10:20 +0200

    re-init app with create-express-api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,30 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import * as middlewares from './middlewares';
+import api from './api';
+import MessageResponse from './interfaces/MessageResponse';
+
+require('dotenv').config();
 
 const app = express();
-const port = 5000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!!');
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use('/api/v1', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+export default app;
