b81247f1c7c39bc293eaac3bf74617e16894248d
chon <balint.ducsai@gmail.com>
2022-09-10 21:33:52 +0200

    add custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   extends: [
     'airbnb',
@@ -12,12 +19,20 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
+  rules: {
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-curly-brace-presence': 0,
+    'react/require-default-props': [
+      2,
+      {
+        functions: 'defaultArguments',
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react-hooks/exhaustive-deps': 1,
+    '@typescript-eslint/no-shadow': 0,
+    'prettier/prettier': 1,
   },
-  rules: {},
-}
+};
