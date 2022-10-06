29209468e1fda2f22a31aac371a88356a1f00ff7
chon <balint.ducsai@gmail.com>
2022-10-06 15:14:37 +0200

    redesign database

--- db/papirsarkany.sql
+++ db/papirsarkany.sql
@@ -1,53 +1,69 @@
-  CREATE TYPE "status" AS ENUM (
-    'Pending',
-    'Canceled',
-    'Fullfilled'
-  );
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-  CREATE TABLE IF NOT EXISTS "customers" (
-    "id" SERIAL PRIMARY KEY,
-    "first_name" text NOT NULL,
-    "last_name" text NOT NULL,
-    "zip_code" text NOT NULL,
-    "city" text NOT NULL,
-    "address" text NOT NULL
-  );
+CREATE TYPE "Status" AS ENUM (
+  'Pending',
+  'Canceled',
+  'Fullfilled'
+);
 
-  CREATE TABLE IF NOT EXISTS "orders" (
-    "id" SERIAL PRIMARY KEY,
-    "customer_id" integer NOT NULL,
-    "status" status NOT NULL,
-    "created_at" timestamp(6) without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
+CREATE TYPE "Category" AS ENUM (
+  'Zsinórtartók',
+  'Pálcák, rudak',
+  'Karboncsövek',
+  'Trevira zsinórok'
+);
 
-  CREATE TABLE IF NOT EXISTS "order_items" (
-    "id" SERIAL PRIMARY KEY,
-    "kite_id" integer NOT NULL,
-    "material_id" integer NOT NULL,
-    "order_id" integer NOT NULL,
-    "quantity" integer NOT NULL
-  );
+CREATE TABLE IF NOT EXISTS "customers" (
+  "id" SERIAL PRIMARY KEY,
+  "first_name" text NOT NULL,
+  "last_name" text NOT NULL,
+  "zip_code" text NOT NULL,
+  "city" text NOT NULL,
+  "address" text NOT NULL
+);
 
-  CREATE TABLE IF NOT EXISTS "kites" (
-    "id" SERIAL PRIMARY KEY,
-    "name" text NOT NULL,
-    "image_url" text NOT NULL,
-    "dimensions" text NOT NULL,
-    "materials" text NOT NULL,
-    "wind" text NOT NULL,
-    "isBeginner" boolean NOT NULL,
-    "details" text NOT NULL,
-    "price" integer NOT NULL
-  );
+CREATE TABLE IF NOT EXISTS "orders" (
+  "id" SERIAL PRIMARY KEY,
+  "customer_id" integer NOT NULL,
+  "status" "Status" NOT NULL,
+  "created_at" timestamp(6) NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
 
-  CREATE TABLE IF NOT EXISTS "materials" (
-    
-  );
+CREATE TABLE IF NOT EXISTS "order_items" (
+  "id" SERIAL PRIMARY KEY,
+  "order_id" integer NOT NULL,
+  "kite_id" UUID NOT NULL,
+  "material_id" UUID NOT NULL,
+  "quantity" integer NOT NULL
+);
 
-  ALTER TABLE "orders" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("id");
+CREATE TABLE IF NOT EXISTS "kites" (
+  "id" UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  "name" text NOT NULL,
+  "image_url" text,
+  "dimensions" text NOT NULL,
+  "materials" text NOT NULL,
+  "wind" text NOT NULL,
+  "is_beginner" boolean NOT NULL,
+  "details" text,
+  "price" integer NOT NULL
+);
 
-  ALTER TABLE "order_items" ADD FOREIGN KEY ("kite_id") REFERENCES "kites" ("id");
+CREATE TABLE IF NOT EXISTS "materials" (
+  "id" UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  "name" text NOT NULL,
+  "image_url" text,
+  "category" "Category" NOT NULL,
+  "diameter" integer,
+  "available_length" integer[],
+  "details" text,
+  "price" integer NOT NULL
+);
 
-  ALTER TABLE "order_items" ADD FOREIGN KEY ("material_id") REFERENCES "materials" ("id");
+ALTER TABLE "orders" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("id");
 
-  ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+ALTER TABLE "order_items" ADD FOREIGN KEY ("kite_id") REFERENCES "kites" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("material_id") REFERENCES "materials" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
