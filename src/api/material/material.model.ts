51415521073812bf411ee629a4c33d21a1c615b7
chon <balint.ducsai@gmail.com>
2023-06-12 18:05:54 +0200

    fix zod type on material

--- src/api/material/material.model.ts
+++ src/api/material/material.model.ts
@@ -3,10 +3,12 @@ import { Product } from '../product/product.model';
 
 export const Material = Product.extend({
   category: z.literal('Anyag'),
-  details: z.object({
-    materialCategory: z.string(),
-    lengths: z.array(z.string()).optional(),
-  }),
+  details: z
+    .object({
+      materialCategory: z.string(),
+      lengths: z.array(z.string()).optional(),
+    })
+    .optional(),
 });
 
 export type Material = z.infer<typeof Material>;
